d395875758279b0d1444f0d05ffca510ca3863ea
hamzah <hamzah.chaudhry@proton.me>
2026-07-19 10:59:15 -0700

    add gentoo upgrade function

---
+++ fish/functions/upgrade.fish
@@ -0,0 +1,9 @@
+function upgrade
+    doas emaint -a sync; or return $status
+    doas emerge -uDN --with-bdeps=y --backtrack=30 @world; or return $status
+    doas emerge -c; or return $status
+    doas emerge @preserved-rebuild; or return $status
+    doas revdep-rebuild; or return $status
+    doas eclean-dist -d; or return $status
+    doas eclean-kernel -n 1; or return $status
+end
