52f5cddfa2cc877a806719190853a2eb670a7582
hamzah <hamzah.chaudhry@proton.me>
2026-07-19 01:25:35 -0700

    wipe out everything for gentoo dotfiles

--- home/.config/zsh/zshrc.d/10-env.zsh
+++
@@ -1,7 +0,0 @@
-# ==========================================
-# tool environment
-# ==========================================
-# interactive tool preferences.
-
-export MANPAGER="sh -c 'sed -u -e \"s/\x1B\[[0-9;]*m//g; s/.\x08//g\" | bat -l man -p --paging=always --pager \"less -R -i --incsearch\"'"
-export LESSHISTFILE=-
