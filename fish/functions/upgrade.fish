a2922008952d5c6e49b1ea582a7e2646f591fd8d
hamzah <hamzah.chaudhry@proton.me>
2026-07-31 18:30:24 -0700

    chore: refine upgrade output and Mango input settings

--- fish/functions/upgrade.fish
+++ fish/functions/upgrade.fish
@@ -1,8 +1,8 @@
 function upgrade
     doas emerge --sync --jobs=2; or return
-    doas emerge -uDN --keep-going @world; or return
+    doas emerge -vuDN --keep-going @world; or return
     doas emerge @preserved-rebuild; or return
-    doas emerge -c; or return
+    doas emerge -vc; or return
     doas eclean-dist -d; or return
     doas eclean-kernel -n 1; or return
     fisher update
--- mango/scripts/caps-osd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-for led in /sys/class/leds/*::capslock/brightness; do
-  read -r state 2>/dev/null < "$led" || continue
-  [ "$state" = 0 ] || exec notify-send -c on -i input-keyboard "Caps Lock" "Enabled"
-done
-
-exec notify-send -c off -i input-keyboard "Caps Lock" "Disabled"
