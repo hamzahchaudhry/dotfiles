35b50fb07ea25d79041ca0f02d9655f120886dfd
hamzah <hamzah.chaudhry@proton.me>
2026-08-19 14:23:20 -0700

    font changes

--- mango/scripts/emoji.sh
+++ mango/scripts/emoji.sh
@@ -2,7 +2,7 @@
 
 choice=$(
   awk -F '# ' '/; fully-qualified/ { sub(/ E[0-9.]+ /, " ", $2); print $2 }' /usr/share/unicode/emoji/emoji-test.txt |
-    tofi --font 'JetBrainsMono Nerd Font, Noto Color Emoji' --prompt-text 'emoji: '
+    tofi --font 'JetBrainsMonoNL Nerd Font, Noto Color Emoji' --prompt-text 'emoji: '
 )
 
 [ -n "$choice" ] && printf '%s' "${choice%% *}" | wl-copy
